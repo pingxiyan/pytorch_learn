5e2796902423681533763dba0c7fe8b84368c4c8
xipingya <xiping.yan@intel.com>
2019-07-26 15:18:08 +0800

    add test local image,but result don't is not correct

--- train_cnn_cifar10/test.py
+++ train_cnn_cifar10/test.py
@@ -36,7 +36,7 @@ def imshow(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
     
-def main(mid_model_name):
+def test(mid_model_name):
     print("=================================================")
     print("test model:", mid_model_name)
     
@@ -51,30 +51,75 @@ def main(mid_model_name):
     
     print(len(images), type(images[0]))
     
-    torchvision.utils.save_image(images[0], "ttt.bmp")
+    torchvision.utils.save_image(images[0], "test_image.bmp")
     
-    
-    file = open("xxx.dat", "w") 
-    for c in range(3):
-        for h in range(32):
-            for w in range(32):
-                #print(images[0][c][h][w])
-                file.write(str(float(images[0][c][h][w])) + ",")  
-            file.write("\n")  
-    file.close() 
-
+    # file = open("xxx.dat", "w") 
+    # for c in range(3):
+    #     for h in range(32):
+    #         for w in range(32):
+    #             #print(images[0][c][h][w])
+    #             file.write(str(float(images[0][c][h][w])) + ",")  
+    #         file.write("\n")  
+    # file.close() 
     
     outputs = net(images)
     
-    imshow(torchvision.utils.make_grid(images))
+    # imshow(torchvision.utils.make_grid(images))
     print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
         
     prob, predicted = torch.max(outputs, 1)
     
     print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(batch_size)))
     print('Predicted: ', ' '.join('%2.2f' % prob[j] for j in range(batch_size)))
+
+import cv2
+import numpy as np
+from numpy import float32
+
+def get_local_image():
+    img = cv2.imread("test_image.bmp")
+
+    print(img.shape)
+    data = np.vstack(img).reshape(-1, 3, 32, 32)
+    data = data.astype(float32)
+    data = (data / 255.0 - 0.5)/0.5
     
+    # cv2.imshow("x", img)
+    # cv2.waitKey(0)
+    # exit(0)
+    torch_tensor = torch.from_numpy(data)
+
+    print(torch_tensor.dtype)
+
+    # print(torch_tensor.shape)
+    # exit(0)
+    return torch_tensor
+
+def test_local_bmp(mid_model_name):
+    print("=================================================")
+    print("test model:", mid_model_name)
+    
+    checkpoint = torch.load(mid_model_name)
+    
+    net = Net()
+    net.load_state_dict(checkpoint['model_state_dict'])
+    net.eval()  # must be call
+
+    images = get_local_image();
+
+    outputs = net(images)
+    
+    # imshow(torchvision.utils.make_grid(images))
+    # print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+        
+    prob, predicted = torch.max(outputs, 1)
+    
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(batch_size)))
+    print('Predicted: ', ' '.join('%2.2f' % prob[j] for j in range(batch_size)))
+    
+
 if __name__ == '__main__':
-    mid_model_name = "/home/xiping/mygithub/pytorch_learn/train_cnn_cifar10/output/1_12000_loss_1.2715.pt"
+    mid_model_name = "./output/1_12000_loss_1.2715.pt"
     #mid_model_name = "C:\\SandyWork\\mygithub\\pytorch_learn\\train_cnn_cifar10\\output\\1_12000_loss_1.2831.pt"
-    main(mid_model_name)
+    test(mid_model_name)
+    test_local_bmp(mid_model_name)
